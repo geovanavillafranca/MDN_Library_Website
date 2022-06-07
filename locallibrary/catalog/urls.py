4fc1a4827db32e711682b58a38c2cad868eb166f
Geovana  Villafranca <geovanavillafranca@hotmail.com>
2022-06-07 20:49:50 -0300

    add interface

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,9 +1,10 @@
+from django.urls import path
 
-from django.urls import include, path
-from django.views.generic import RedirectView
+from . import views
+
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
 ]
 
 
